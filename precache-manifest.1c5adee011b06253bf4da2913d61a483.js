d89bf8a3a53abdbfd3120ab17ee98feaa270cf09
vatsalchheda <vatsalchheda007@gmail.com>
2021-12-18 10:06:38 +0530

    Updates

---
+++ precache-manifest.1c5adee011b06253bf4da2913d61a483.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64537ce62967ec7e27a3e77005a46e0b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0d8b36e1e76311669565",
+    "url": "/portfolio/static/css/3.3327a641.chunk.css"
+  },
+  {
+    "revision": "fa9b493c49853344f5ce",
+    "url": "/portfolio/static/css/4.1bf2f3ab.chunk.css"
+  },
+  {
+    "revision": "9bebacc95d92daae00ba",
+    "url": "/portfolio/static/css/main.ab59b855.chunk.css"
+  },
+  {
+    "revision": "b58c5a74f803b5887927",
+    "url": "/portfolio/static/js/2.cd697516.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.cd697516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d8b36e1e76311669565",
+    "url": "/portfolio/static/js/3.40534873.chunk.js"
+  },
+  {
+    "revision": "fa9b493c49853344f5ce",
+    "url": "/portfolio/static/js/4.d820a8c4.chunk.js"
+  },
+  {
+    "revision": "9bebacc95d92daae00ba",
+    "url": "/portfolio/static/js/main.8e129fda.chunk.js"
+  },
+  {
+    "revision": "0f923d4b6d773b5e53d6",
+    "url": "/portfolio/static/js/runtime-main.24415d7b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "34ebdaad90e6972aedd4632f4bd910b8",
+    "url": "/portfolio/static/media/DAV.34ebdaad.png"
+  },
+  {
+    "revision": "a84d951b87b3f276e4b9c484b91d4baf",
+    "url": "/portfolio/static/media/DJSCE.a84d951b.jpg"
+  },
+  {
+    "revision": "069a1c335c9422ca18204a0e9d1b461a",
+    "url": "/portfolio/static/media/MonkE.069a1c33.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "546c77d4fd43aa82ea05605cc8940e33",
+    "url": "/portfolio/static/media/SRJC.546c77d4.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "1a5df81d0f5885e42c258ba8755f93c2",
+    "url": "/portfolio/static/media/munchin.1a5df81d.png"
+  },
+  {
+    "revision": "ecade7502f7ecc90c3f94da3e5f602a9",
+    "url": "/portfolio/static/media/paperautoml.ecade750.png"
+  },
+  {
+    "revision": "fcc7727a0dbfed067ea926b4897aeca6",
+    "url": "/portfolio/static/media/paperbeproj.fcc7727a.png"
+  },
+  {
+    "revision": "5028739e3afb1969d138b81346ae780e",
+    "url": "/portfolio/static/media/papernmt.5028739e.png"
+  },
+  {
+    "revision": "528400119d9887858a03142960975b7b",
+    "url": "/portfolio/static/media/paperstroke.52840011.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "53953ae8f36730230d11bf9c72e53f91",
+    "url": "/portfolio/static/media/unicode.53953ae8.svg"
+  }
+]);
